f8b6c20dfe806d23a2fc48a88542d675c71b49ed
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2018-10-02 19:21:15 -0500

    formatting

--- app/ts/index.ts
+++ app/ts/index.ts
@@ -6,7 +6,7 @@ import EmbarkJS from 'Embark/EmbarkJS';
 // @ts-ignore
 // import SimpleStorage from 'Embark/contracts/SimpleStorage';
 
-import {Greeter} from './greet'
+import {Greeter} from './greet';
 
 // check with `npm run type-check` or `npm run type-check:watch`
 const badlyTyped: string = true;
@@ -14,10 +14,10 @@ const badlyTyped: string = true;
 // can run type checker in one terminal while embark is running in another
 
 EmbarkJS.onReady(function(error: string) {
-  if(error) {
-    console.error(error)
+  if (error) {
+    console.error(error);
   }
-  console.log("Embark is ready")
+  console.log("Embark is ready");
 });
 
 let greeter = new Greeter("world");
